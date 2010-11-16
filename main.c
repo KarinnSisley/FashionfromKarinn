ec90b8128cd3f78c9e338291e11aab86980298d3
Karina <undomielstar@mail.ru>
2010-11-17 00:17:41 +0300

    второй уровень (доработки)

--- main.c
+++ main.c
@@ -49,7 +49,8 @@ Uint32 callback ( Uint32 interval, void*p)
 int main( int argc, char *args [])
 {
     char b[20];
-    int shoulderx=36/2, shouldery=65,x,y,length,height,score=0,quite = 0,nenuzhnaya=1,level;
+
+    int shoulderx=36/2, shouldery=65,x,y,length,height,score=40,quite = 0,nenuzhnaya=1,level;
     TTF_Init();
     SDL_Surface* screen = NULL;
     SDL_Surface *message = NULL;
@@ -66,6 +67,7 @@ int main( int argc, char *args [])
     screen = SDL_SetVideoMode( 1024, 700, 16, SDL_SWSURFACE );
 
 
+    SDL_AddTimer(8000,callback, &nenuzhnaya);
     level=1;
     background = IMG_Load("background1.jpg");
     girl.picture = Load_Image("dita.png");
@@ -133,7 +135,7 @@ int main( int argc, char *args [])
         {
             if (level==1)
             {
-                SDL_AddTimer(8000,callback, &nenuzhnaya);
+
                 if ( !nenuzhnaya && event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
                 {
                     x = event.button.x;
@@ -339,178 +341,176 @@ int main( int argc, char *args [])
                     forbagstxt.offset.y=800/2;
                     forjewelrytxt.offset.x= 3000/5.5;
                     forjewelrytxt.offset.y=800/10;
+                }
 
+            }
+
+
+            if (level==2)
+            {
+                if ( !nenuzhnaya && event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
+                {
+                    x = event.button.x;
+                    y = event.button.y;
+                    length=bag.offset.x + bag.offset.w;
+                    height=bag.offset.y + bag.offset.h;
+                    if (x>bag.offset.x && x<(length) && y>bag.offset.y && y<(height))
+                    {
+                        cloth_selected=&bag;
+                        continue;
+                    }
+
+                    if (cloth_selected==&bag)
+                    {
+                        length=forbags.offset.x + forbags.offset.w;
+                        height=forbags.offset.y + forcoat.offset.h;
+
+                        if (x>forbags.offset.x && x<(length) && y>forbags.offset.y && y<(height))
+                            score=score+10;
+                        bag.flag=0;
+                        cloth_selected=NULL;
+                    }
+                    x= event.button.x;
+                    y = event.button.y;
+                    length=dress.offset.x + dress.offset.w;
+                    height=dress.offset.y + dress.offset.h;
+                    if (x>dress.offset.x && x<(length) && y>dress.offset.y && y<(height))
+                    {
+                        cloth_selected=&dress;
+                        continue;
+                    }
+                    if (cloth_selected==&dress)
+                    {
+
+                        x = event.button.x;
+                        y = event.button.y;
+                        length=fordress.offset.x + fordress.offset.w;
+                        height=fordress.offset.y + fordress.offset.h;
+
+                        if (x>fordress.offset.x && x<(length) && y>fordress.offset.y && y<(height))
+                            score=score+10;
+                        dress.flag=0;
+                        cloth_selected=NULL;
+                    }
+                    length=trousers.offset.x + trousers.offset.w;
+                    height=trousers.offset.y + trousers.offset.h;
+                    if (x>trousers.offset.x && x<(length) && y>trousers.offset.y && y<(height))
+                    {
+                        cloth_selected=&trousers;
+                        continue;
+                    }
+
+                    if (cloth_selected==&trousers)
+                    {
+
+                        x = event.button.x;
+                        y = event.button.y;
+                        length=fortrousers.offset.x + fortrousers.offset.w;
+                        height=fortrousers.offset.y + fortrousers.offset.h;
+
+                        if (x>fortrousers.offset.x && x<(length) && y>fortrousers.offset.y && y<(height))
+                            score=score+10;
+                        trousers.flag=0;
+                        cloth_selected=NULL;
+                    }
+                    length=jewelry.offset.x + jewelry.offset.w;
+                    height=jewelry.offset.y + jewelry.offset.h;
+                    if (x>jewelry.offset.x && x<(length) && y>jewelry.offset.y && y<(height))
+                    {
+                        cloth_selected=&jewelry;
+                        continue;
+                    }
+
+                    if (cloth_selected==&jewelry)
+                    {
+
+                        x = event.button.x;
+                        y = event.button.y;
+                        length=forjewelry.offset.x + forjewelry.offset.w;
+                        height=forjewelry.offset.y + forjewelry.offset.h;
+
+                        if (x>forjewelry.offset.x && x<(length) && y>forjewelry.offset.y && y<(height))
+                            score=score+10;
+                        jewelry.flag=0;
+                        cloth_selected=NULL;
+                    }
+
+                    x = event.button.x;
+                    y = event.button.y;
+                    length=shoes.offset.x + shoes.offset.w;
+                    height=shoes.offset.y + shoes.offset.h;
+                    if (x>shoes.offset.x && x<(length) && y>shoes.offset.y && y<(height))
+                    {
+                        cloth_selected=&shoes;
+                        continue;
+                    }
+
+                    if (cloth_selected==&shoes)
+                    {
+
+                        x = event.button.x;
+                        y = event.button.y;
+                        length=forshoes.offset.x + forshoes.offset.w;
+                        height=forshoes.offset.y + forshoes.offset.h;
+
+                        if (x>forshoes.offset.x && x<(length) && y>forshoes.offset.y && y<(height))
+                            score=score+10;
+                        shoes.flag=0;
+                        cloth_selected=NULL;
+                    }
 
 
                 }
+                if ( event.type == SDL_QUIT )
+                    quite = 1;
+                SDL_AddTimer(10000,callback, &nenuzhnaya);
+                SDL_BlitSurface( background, NULL, screen, NULL );
+                blitcloth (&girl, screen);
+                blitcloth (&trousers, screen);
+                blitcloth (&dress, screen);
+                blitcloth (&shoes,screen);
+                blitcloth (&bag, screen);
+                blitcloth (&jewelry, screen);
+                blitcloth (&fortrousers,screen);
+                blitcloth (&fordress,screen);
+                blitcloth (&forshoes, screen);
+                blitcloth (&forbags, screen);
+                blitcloth (&forjewelry, screen);
+                if (nenuzhnaya)
+                {
+                    blitcloth (&forshoestxt,screen);
+                    blitcloth (&fortrouserstxt,screen);
+                    blitcloth (&fordressestxt,screen);
+                    blitcloth (&forbagstxt,screen);
+                    blitcloth (&forjewelrytxt,screen);
+                }
+                snprintf(b, sizeof (b), "score: %d", score);
+                SDL_Color textColor = { 50, 205, 50 };
+                message = TTF_RenderText_Solid( font, b, textColor );
+                text.x= 900/2;
+                text.y=700/18;
+                text.w=message->w;
+                text.h=message->h;
+                SDL_BlitSurface( message, NULL,screen, &text );
+                SDL_Flip( screen );
+
+                if (score<90 && dress.flag==0 && bag.flag==0 && trousers.flag==0 && shoes.flag==0 && jewelry.flag==0)
+                {
+                    dress.flag=1;
+                    bag.flag=1;
+                    trousers.flag=1;
+                    shoes.flag=1;
+                    jewelry.flag=1;
+                    score=40;
+                    continue;
+                }
+                else if (score==90)
+                    level=3;
             }
+
+
         }
 
-
-        if (level==2)
-        {
-            if ( !nenuzhnaya && event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
-            {
-                x = event.button.x;
-                y = event.button.y;
-                length=bag.offset.x + bag.offset.w;
-                height=bag.offset.y + bag.offset.h;
-                if (x>bag.offset.x && x<(length) && y>bag.offset.y && y<(height))
-                {
-                    cloth_selected=&bag;
-                    continue;
-                }
-
-                if (cloth_selected==&bag)
-                {
-                    length=forbags.offset.x + forbags.offset.w;
-                    height=forbags.offset.y + forcoat.offset.h;
-
-                    if (x>forbags.offset.x && x<(length) && y>forbags.offset.y && y<(height))
-                        score=score+10;
-                    bag.flag=0;
-                    cloth_selected=NULL;
-                }
-                x= event.button.x;
-                y = event.button.y;
-                length=dress.offset.x + dress.offset.w;
-                height=dress.offset.y + dress.offset.h;
-                if (x>dress.offset.x && x<(length) && y>dress.offset.y && y<(height))
-                {
-                    cloth_selected=&dress;
-                    continue;
-                }
-                if (cloth_selected==&dress)
-                {
-
-                    x = event.button.x;
-                    y = event.button.y;
-                    length=fordress.offset.x + fordress.offset.w;
-                    height=fordress.offset.y + fordress.offset.h;
-
-                    if (x>fordress.offset.x && x<(length) && y>fordress.offset.y && y<(height))
-                        score=score+10;
-                    dress.flag=0;
-                    cloth_selected=NULL;
-                }
-                length=trousers.offset.x + trousers.offset.w;
-                height=trousers.offset.y + trousers.offset.h;
-                if (x>trousers.offset.x && x<(length) && y>trousers.offset.y && y<(height))
-                {
-                    cloth_selected=&trousers;
-                    continue;
-                }
-
-                if (cloth_selected==&trousers)
-                {
-
-                    x = event.button.x;
-                    y = event.button.y;
-                    length=fortrousers.offset.x + fortrousers.offset.w;
-                    height=fortrousers.offset.y + fortrousers.offset.h;
-
-                    if (x>fortrousers.offset.x && x<(length) && y>fortrousers.offset.y && y<(height))
-                        score=score+10;
-                    trousers.flag=0;
-                    cloth_selected=NULL;
-                }
-                length=jewelry.offset.x + jewelry.offset.w;
-                height=jewelry.offset.y + jewelry.offset.h;
-                if (x>jewelry.offset.x && x<(length) && y>jewelry.offset.y && y<(height))
-                {
-                    cloth_selected=&jewelry;
-                    continue;
-                }
-
-                if (cloth_selected==&jewelry)
-                {
-
-                    x = event.button.x;
-                    y = event.button.y;
-                    length=forjewelry.offset.x + forjewelry.offset.w;
-                    height=forjewelry.offset.y + forjewelry.offset.h;
-
-                    if (x>forjewelry.offset.x && x<(length) && y>forjewelry.offset.y && y<(height))
-                        score=score+10;
-                    jewelry.flag=0;
-                    cloth_selected=NULL;
-                }
-
-                x = event.button.x;
-                y = event.button.y;
-                length=shoes.offset.x + shoes.offset.w;
-                height=shoes.offset.y + shoes.offset.h;
-                if (x>shoes.offset.x && x<(length) && y>shoes.offset.y && y<(height))
-                {
-                    cloth_selected=&shoes;
-                    continue;
-                }
-
-                if (cloth_selected==&shoes)
-                {
-
-                    x = event.button.x;
-                    y = event.button.y;
-                    length=forshoes.offset.x + forshoes.offset.w;
-                    height=forshoes.offset.y + forshoes.offset.h;
-
-                    if (x>forshoes.offset.x && x<(length) && y>forshoes.offset.y && y<(height))
-                        score=score+10;
-                    shoes.flag=0;
-                    cloth_selected=NULL;
-                }
-
-
-            }
-            if ( event.type == SDL_QUIT )
-                quite = 1;
-            SDL_AddTimer(10000,callback, &nenuzhnaya);
-            SDL_BlitSurface( background, NULL, screen, NULL );
-            blitcloth (&girl, screen);
-            blitcloth (&trousers, screen);
-            blitcloth (&dress, screen);
-            blitcloth (&shoes,screen);
-            blitcloth (&bag, screen);
-            blitcloth (&jewelry, screen);
-            blitcloth (&fortrousers,screen);
-            blitcloth (&fordress,screen);
-            blitcloth (&forshoes, screen);
-            blitcloth (&forbags, screen);
-            blitcloth (&forjewelry, screen);
-            if (nenuzhnaya)
-            {
-                blitcloth (&forshoestxt,screen);
-                blitcloth (&fortrouserstxt,screen);
-                blitcloth (&fordressestxt,screen);
-                blitcloth (&forbagstxt,screen);
-                blitcloth (&forjewelrytxt,screen);
-            }
-            snprintf(b, sizeof (b), "score: %d", score);
-            SDL_Color textColor = { 50, 205, 50 };
-            message = TTF_RenderText_Solid( font, b, textColor );
-            text.x= 900/2;
-            text.y=700/18;
-            text.w=message->w;
-            text.h=message->h;
-            SDL_BlitSurface( message, NULL,screen, &text );
-            SDL_Flip( screen );
-
-            if (score<90 && dress.flag==0 && bag.flag==0 && trousers.flag==0 && shoes.flag==0 && jewelry.flag==0)
-            {
-                dress.flag=1;
-                bag.flag=1;
-                trousers.flag=1;
-                shoes.flag=1;
-                jewelry.flag=1;
-                score=40;
-                continue;
-            }
-            else if (score=90)
-                level=3;
-
-
     }
-
-}
-return 0;
+    return 0;
 }
